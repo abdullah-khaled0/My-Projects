bf1f8c3f8d9f221bb532b15990b42efda15806f0
Abdullah Khaled <130568489+abdullah-khaled0@users.noreply.github.com>
2023-04-30 13:39:24 +0200

    Add files via upload

---
+++ Data Analytics Projects/Streamlit Apps/happy-sad-classification/happy-sad-classification.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+
+# Define a function to load the model and cache it
+@st.cache_resource()
+def load_model():
+    # Load the pre-trained model
+    model = tf.keras.models.load_model('imageclassifier.h5')
+    return model
+
+# Load the model and cache it
+model = load_model()
+
+# Define the Streamlit app
+def app():
+    # Set the app title
+    st.title('Happy😄 or Sad🥺')
+
+    # Add a file uploader widget
+    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+    # Check if an image has been uploaded
+    if uploaded_file is not None:
+        # Read the image data
+        image = Image.open(uploaded_file)
+
+        # Resize the image to match the expected input shape of the model
+        image = image.resize((256, 256))
+
+        # Convert the image to a numpy array
+        image_array = np.array(image)
+
+        # Normalize the pixel values
+        image_array = image_array / 255.0
+
+        # Add a batch dimension
+        image_array = np.expand_dims(image_array, axis=0)
+
+        # Make predictions on the image
+        predictions = model.predict(image_array)
+
+        if predictions >= 0.5: 
+            predicted_class_name = 'sad🥺'
+        else:
+            predicted_class_name = 'happy😄'
+
+        # Display the image and the predicted class name
+        st.image(image, caption='', use_column_width=True)
+
+        # Add a separator
+        st.write('---')
+
+        # Display the predicted class name
+        st.markdown(f'## {predicted_class_name.capitalize()}')
+# Run the app
+if __name__ == "__main__":
+    app()
