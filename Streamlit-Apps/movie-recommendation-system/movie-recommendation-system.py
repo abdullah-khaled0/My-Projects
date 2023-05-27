3dda6bfe983511d212b6aae95c5d88b4a6378b9a
Abdullah Khaled <130568489+abdullah-khaled0@users.noreply.github.com>
2023-05-27 10:38:40 +0300

    Update movie-recommendation-system.py

--- Streamlit-Apps/movie-recommendation-system/movie-recommendation-system.py
+++ Streamlit-Apps/movie-recommendation-system/movie-recommendation-system.py
@@ -15,7 +15,7 @@ movie_name = st.text_input("Enter Movie's name")
 @st.cache_resource
 def load_data():
     # Load the movies data from CSV
-    movies_data = pd.read_csv("d:/My_Projects/Jupyter/Streamlit-Apps/movie-recommendation-system/movies.csv")
+    movies_data = pd.read_csv("movies.csv")
 
     # Select the relevant features for recommendation
     selected_features = ['genres','keywords','tagline','cast','director']
