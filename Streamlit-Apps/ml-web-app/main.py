94fc2eab64b337ccf6d13b4b2dc5fe8a6a85ce2b
Abdullah Khaled <130568489+abdullah-khaled0@users.noreply.github.com>
2023-07-15 19:31:01 +0300

    Update main.py

--- Streamlit-Apps/ml-web-app/main.py
+++ Streamlit-Apps/ml-web-app/main.py
@@ -8,5 +8,8 @@ st.title('Data + ML + Streamlit = Power')
 
 csv_file = st.file_uploader("Upload CSV", type=["csv"])
 
+if st.button('Load Default Data'):
+    csv_file = "Streamlit-Apps\ml-web-app\diabetes.csv"
+
 if csv_file is not None:
     body(csv_file)
